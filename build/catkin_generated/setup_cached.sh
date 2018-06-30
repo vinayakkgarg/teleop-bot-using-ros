fb01fb41ce78aea58eda36890704a8a532455fd7
vinayakkgarg <vinayakk.garg99@gmail.com>
2018-07-01 02:34:41 +0530

    adding all files

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/vinayakk/teleopbot_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda-8.0/lib64:/usr/local/cuda/lib64"
+export PWD="/home/vinayakk/teleopbot_ws/build"
+export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages:/usr/lib/python2.7/dist-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/vinayakk/teleopbot_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/vinayakk/teleopbot_ws/src:$ROS_PACKAGE_PATH"
---
+++ build/teleopbot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vinayakk/teleopbot_ws/src/teleopbot
+# Build directory: /home/vinayakk/teleopbot_ws/build/teleopbot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
